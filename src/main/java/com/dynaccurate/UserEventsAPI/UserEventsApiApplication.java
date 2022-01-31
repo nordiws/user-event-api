b1e79eec21670050045a49ee4f7772c8c1b745a4
Pablo <nordiws@gmail.com>
2022-01-31 17:25:49 -0300

    Initial commit

---
+++ src/main/java/com/dynaccurate/UserEventsAPI/UserEventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynaccurate.UserEventsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEventsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynaccurate/UserEventsAPI/UserEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynaccurate.UserEventsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
