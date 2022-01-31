5b2e9ec0552f760296f6e4fcc728016131848197
Pablo <nordiws@gmail.com>
2022-01-31 20:38:33 -0300

    deleting unused dependency

--- src/main/java/com/dynaccurate/UserEventsAPI/repository/UserRepository.java
+++ src/main/java/com/dynaccurate/UserEventsAPI/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.dynaccurate.UserEventsAPI.repository;
 
-import java.util.Optional;
-
 import com.dynaccurate.UserEventsAPI.models.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
